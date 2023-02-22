11152760e061c1b99890f191fa034dfbe58551fc
GBbartosz <bartosz.grygalewicz@gmail.com>
2023-02-23 00:34:05 +0100

    23.02.2023

--- update.py
+++ update.py
@@ -139,16 +139,36 @@ def check_positions(ticker, url, positions_dic, num, df):
             return True
 
 
+def get_rid_of_index_and_indicators(coll):
+    if 'index' in coll:
+        coll.remove('index')
+    for c in coll:
+        if 'Fiscal' in c:
+            coll.remove(c)
+    return coll
+
+
 def common_member(a, b):
-    a_set = set(a)
-    b_set = set(b)
-    return list(a_set & b_set)
+    common = []
+    for c in a:
+        if c in b:
+            common.append(c)
+    common = get_rid_of_index_and_indicators(common)
+    return common
+
+
+def get_columns_only_in_sql(df, sql_df):
+    res = []
+    for sqlc in sql_df.columns:
+        if sqlc not in df.columns:
+            res.append(sqlc)
+    res = get_rid_of_index_and_indicators(res)
+    return res
 
 
 def update_sql_table_empty_values(df, sql_df, cursor, table_name):
     common_columns = common_member(df.columns, sql_df.columns)
     if common_columns:
-
         for col in common_columns:
             for r in sql_df.index:
                 if sql_df.loc[r, col] == '-' and df.loc[r, col] != '-'\
@@ -161,6 +181,85 @@ def update_sql_table_empty_values(df, sql_df, cursor, table_name):
                                     WHERE [index] = {}
                                     '''.format(table_name, col, new_val, r)
                     cursor.execute(sql_update_statement)
+
+
+def avg(mylist):
+    return sum(mylist) / len(mylist)
+
+
+def get_name_of_column_with_indicator_names(tdf):
+    column_name = tdf.columns[1]
+    return column_name
+
+
+def shorten_df_to_chosen_fragment(tdf, chosen_columns, column_name, share_ind):
+    tdf = tdf.query("`{}` == '{}'".format(column_name, share_ind))
+    tdf = tdf[chosen_columns]
+    tdf = squeezing(tdf)
+    return tdf
+
+
+def compare_changes(changel):
+    outcome = True
+    c0 = changel[0]
+    for c in changel:
+        res = c / c0
+        if res < 0.9 or res > 1.1:
+            print('Error stock split change: {}'.format(changel))
+            outcome = False
+    return outcome
+
+
+def squeezing(myseries):
+    try:
+        x = myseries.squeeze().tolist()
+    except AttributeError:
+        x = list(myseries.squeeze())
+    return x
+
+
+def update_sql_table_diluted_shares_outstanding(df, sql_df, cursor, table_name):
+    common_columns = common_member(df.columns, sql_df.columns)
+    only_sql_columns = get_columns_only_in_sql(df, sql_df)
+    df_indicators_column_name = get_name_of_column_with_indicator_names(df)
+    sql_df_indicators_column_name = get_name_of_column_with_indicator_names(sql_df)
+    shares_indicators = ['Basic Shares Outstanding', 'Diluted Shares Outstanding', 'EPS (Basic)']
+    diff_dict = {}
+    if common_columns:
+        for share_ind in shares_indicators:
+            s_l = shorten_df_to_chosen_fragment(df, common_columns, df_indicators_column_name, share_ind)
+            ssql_l = shorten_df_to_chosen_fragment(sql_df, common_columns, sql_df_indicators_column_name, share_ind)
+            diff_l = []
+            for v, sqlv, col in zip(s_l, ssql_l, common_columns):
+                if v != sqlv:
+                    if v != '-' and sqlv != '-':
+                        diff_l.append(float(v) / float(sqlv))
+                    sql_update_statement = '''
+                                    UPDATE wsj.dbo.{}
+                                    SET [{}] = '{}'
+                                    WHERE [{}] = '{}'
+                                    '''.format(table_name, col, v, sql_df_indicators_column_name, share_ind)
+                    #cursor.execute(sql_update_statement)
+            diff_dict[share_ind] = diff_l
+
+    if len(only_sql_columns) > 0:
+        for share_ind in shares_indicators:
+            if diff_dict[share_ind]:
+                if compare_changes(diff_dict[share_ind]) is False:
+                    break
+                change = avg(diff_dict[share_ind])
+                ssql_l = shorten_df_to_chosen_fragment(sql_df, only_sql_columns, sql_df_indicators_column_name, share_ind)
+                print('x')
+                print(ssql_l)
+                for sqlv, col in zip(ssql_l, only_sql_columns):
+                    new_v = float(sqlv) * change
+                    sql_update_statement = '''
+                                    UPDATE wsj.dbo.{}
+                                    SET [{}] = '{}'
+                                    WHERE [{}] = '{}'
+                                    '''.format(table_name, col, new_v, sql_df_indicators_column_name, share_ind)
+                    print(sql_update_statement)
+                    # cursor.execute(sql_update_statement)
 
 
 def update(ticker, ticker_tables):
@@ -322,7 +421,9 @@ def update(ticker, ticker_tables):
             sql_headers_names = list(sql_df.columns)
             lacking_column_names = []
             update_sql_table_empty_values(df, sql_df, cursor, ticker_tables[num])
-            #update_sql_table_diluted_shares_outstanding(df, sql_df)
+            #income statement tables q, y
+            if ticker_tables[num] in [ticker_tables[0], ticker_tables[1]]:
+                update_sql_table_diluted_shares_outstanding(df, sql_df, cursor, ticker_tables[num])
 
             if len(check_if_columns_are_current(web_headers_names, sql_headers_names, lacking_column_names)) == 0:
                 pass

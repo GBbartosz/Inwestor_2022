a8e889bf274f5900fc6c4f668ab05bf481f3b075
GBbartosz <bartosz.grygalewicz@gmail.com>
2023-05-18 00:43:30 +0200

    18.05.2023

--- indicatorall.py
+++ indicatorall.py
@@ -1,5 +1,5 @@
 import utilities as u
-
+from tickerclass import Ticker
 
 def get_all_analysis_tables(cursor):
     sql_table_list = u.get_all_tables(cursor)
@@ -8,17 +8,49 @@ def get_all_analysis_tables(cursor):
 
 
 class IndicatorAll:
-    def __init__(self, mydate, wsj_cursor, wsj_conn, wsj_engine, wsja_cursor, wsja_conn, wsja_engine):
+    def __init__(self, tickers_l, wsj_cursor, wsj_conn, wsj_engine, wsja_cursor, wsja_conn, wsja_engine):
+        self.tickers_l = tickers_l
         self.df_y = None
         self.df_q = None
         self.wsj_cursor, self.wsj_conn, self.wsj_engine = wsj_cursor, wsj_conn, wsj_engine
         self.wsja_cursor, self.wsja_conn, self.wsja_engine = wsja_cursor, wsja_conn, wsja_engine
+        self.tic = None
+        self.statement = None
+        self.period = None
 
 
-
-
-
-
+    def set_data(self):
+        if self.statement == 'analysis' and self.period == 'year':
+            self.tic.set_df_year()
+        elif self.statement == 'analysis' and self.period == 'quarter':
+            self.tic.set_df_quarter()
+        elif self.statement == 'is' and self.period == 'year':
+            self.tic.set_is_df_y()
+        elif self.statement == 'is' and self.period == 'quarter':
+            self.tic.set_is_df_q()
+        elif self.statement == 'ba' and self.period == 'year':
+            self.tic.set_ba_df_y()
+        elif self.statement == 'ba' and self.period == 'quarter':
+            self.tic.set_ba_df_q()
+        elif self.statement == 'bl' and self.period == 'year':
+            self.tic.set_bl_df_y()
+        elif self.statement == 'bl' and self.period == 'quarter':
+            self.tic.set_bl_df_q()
+        elif self.statement == 'cf' and self.period == 'year':
+            self.tic.set_cf_df_y()
+        elif self.statement == 'cf' and self.period == 'quarter':
+            self.tic.set_cf_df_q()
+        
+    def update(self, statement, period):
+        self.statement = statement
+        self.period = period
+        
+    def get_data(self):
+        for tic_name in tickers_l:
+            self.tic = Ticker(tic_name, self.wsj_cursor, self.wsj_conn, self.wsj_engine, self.wsja_cursor, self.wsja_conn, 
+                         self.wsja_engine)
+            
+        
     #def indicator(self):
     #    class Indicator:
     #        def __init__(self, indicator, indicators_dict, tickers_list, wsj_conn):
